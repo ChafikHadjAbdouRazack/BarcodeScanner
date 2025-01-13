72a161ce5c20566383a788a82c6406516364002f
Chafik HADJ ABDOU RAZACK <chafikhadjabdourazack@gmail.com>
2025-01-13 23:19:30 +0100

    Initial commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Chafik HADJ ABDOU RAZACK on 13/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarcodeScannerTests/BarcodeScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerTests.swift
+//  BarcodeScannerTests
+//
+//  Created by Chafik HADJ ABDOU RAZACK on 13/01/2025.
+//
+
+import Testing
+@testable import BarcodeScanner
+
+struct BarcodeScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
